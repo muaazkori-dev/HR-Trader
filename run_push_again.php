2f727f5bda5864ae0467147fb4e280b8953a1f65
muaazkori-dev <muaazkori-dev@users.noreply.github.com>
2026-06-27 06:57:17 -0700

    Auto-update: Configured checkout login prompts and automatic status update notifications

--- run_push_again.php
+++ run_push_again.php
@@ -1,6 +1,4 @@
 <?php
-header('Content-Type: text/plain; charset=utf-8');
-echo "=== RUNNING GIT PUSH FOR MARQUEE REMOVAL ===\n";
-$output = shell_exec("cmd /c \"d:\\xampp orginal\\htdocs\\HR Traders\\git_push.bat\" 2>&1");
-echo $output;
+http_response_code(404);
+echo "404 Not Found";
 ?>
